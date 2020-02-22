2026b83371840819405730a5706b6baccfbd16ce
陈朵拉 <conan8chan@yahoo.com>
2020-02-22 19:09:47 +0800

    helloRedis2 intellij

--- gapi-gateway/src/main/java/io/github/example/graphql/subscription/HelloSubscriptionResolver.java
+++ gapi-gateway/src/main/java/io/github/example/graphql/subscription/HelloSubscriptionResolver.java
@@ -1,8 +1,10 @@
 package io.github.example.graphql.subscription;
 
 import java.util.Map;
+
 import com.coxautodev.graphql.tools.GraphQLSubscriptionResolver;
 import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.connection.stream.MapRecord;
 import org.springframework.data.redis.connection.stream.StreamOffset;
@@ -13,20 +15,34 @@ import reactor.core.publisher.Flux;
 
 @Service
 public class HelloSubscriptionResolver implements GraphQLSubscriptionResolver {
-  
-	@Autowired StreamReceiver<String, MapRecord<String, String, String>> streamReceiver;
 
-  public Publisher<String> helloWorld() {
-    //TODO: https://spring.io/guides/gs/spring-data-reactive-redis/
-    return  Flux.just("Hello", "World!");
-  }
+    @Autowired
+    StreamReceiver<String, MapRecord<String, String, String>> streamReceiver;
 
-  public Publisher<String> helloRedis() {
-    final Flux<MapRecord<String, String, String>> messages = streamReceiver.receive(StreamOffset.fromStart(SensorData.KEY));
-    
-    Flux<Map<String,String>> vs = messages.map(MapRecord::getValue);
-    
-    return  vs.map(aa->aa.values().toString());
-  }
-    
+    public Publisher<String> helloWorld() {
+        //TODO: https://spring.io/guides/gs/spring-data-reactive-redis/
+        return Flux.just("Hello", "World!");
+    }
+
+    public Publisher<String> helloRedis() {
+        final Flux<MapRecord<String, String, String>> messages = streamReceiver.receive(StreamOffset.fromStart(SensorData.KEY));
+
+        Flux<Map<String, String>> vs = messages.map(MapRecord::getValue);
+
+        return vs.map(aa -> aa.values().toString());
+    }
+
+    public Publisher<String> helloRedis2() {
+        final Flux<MapRecord<String, String, String>> messages = streamReceiver.receive(StreamOffset.fromStart(SensorData.KEY));
+
+        Flux<Map<String, String>> vs = messages.map(MapRecord::getValue);
+
+        return vs.switchMap(v -> new Publisher<String>(){
+            @Override
+            public void subscribe(Subscriber<? super String> s) {
+                s.onNext(v.values().toString());
+            }
+        } );
+    }
+
 }
