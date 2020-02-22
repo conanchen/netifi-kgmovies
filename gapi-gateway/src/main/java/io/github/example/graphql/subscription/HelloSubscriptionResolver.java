0982e8de76fd5d8f211dcd5d753d1cb9a91e0c36
陈朵拉 <conan8chan@yahoo.com>
2020-02-22 11:09:39 +0800

    redis-stream

--- gapi-gateway/src/main/java/io/github/example/graphql/subscription/HelloSubscriptionResolver.java
+++ gapi-gateway/src/main/java/io/github/example/graphql/subscription/HelloSubscriptionResolver.java
@@ -1,18 +1,32 @@
 package io.github.example.graphql.subscription;
 
+import java.util.Map;
 import com.coxautodev.graphql.tools.GraphQLSubscriptionResolver;
-
 import org.reactivestreams.Publisher;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.stream.MapRecord;
+import org.springframework.data.redis.connection.stream.StreamOffset;
+import org.springframework.data.redis.stream.StreamReceiver;
 import org.springframework.stereotype.Service;
-
+import io.github.example.springdata.redis.SensorData;
 import reactor.core.publisher.Flux;
 
 @Service
 public class HelloSubscriptionResolver implements GraphQLSubscriptionResolver {
+  
+	@Autowired StreamReceiver<String, MapRecord<String, String, String>> streamReceiver;
 
-    public Publisher<String> helloWorld() {
-      //TODO: https://spring.io/guides/gs/spring-data-reactive-redis/
-      return  Flux.just("Hello", "World!");
-    }
+  public Publisher<String> helloWorld() {
+    //TODO: https://spring.io/guides/gs/spring-data-reactive-redis/
+    return  Flux.just("Hello", "World!");
+  }
+
+  public Publisher<String> helloRedis() {
+    final Flux<MapRecord<String, String, String>> messages = streamReceiver.receive(StreamOffset.fromStart(SensorData.KEY));
+    
+    Flux<Map<String,String>> vs = messages.map(MapRecord::getValue);
+    
+    return  vs.map(aa->aa.values().toString());
+  }
+    
 }
