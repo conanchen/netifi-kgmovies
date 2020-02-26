a2f053d13be630d274c3c46f62780edbb623a95c
陈朵拉 <conan8chan@yahoo.com>
2020-02-26 12:07:56 +0800

    Store an Organization

--- gapi-gateway/src/main/java/io/github/conanchen/place/graphql/model/ChildcareGQO.java
+++ gapi-gateway/src/main/java/io/github/conanchen/place/graphql/model/ChildcareGQO.java
@@ -1,22 +1,45 @@
 package io.github.conanchen.place.graphql.model;
 
+import io.github.conanchen.action.graphql.model.ActionGQO;
+import io.github.conanchen.personorg.graphql.model.MembershipConnectionGQO;
+import io.github.conanchen.personorg.graphql.model.MembershipGQO;
+import io.github.conanchen.personorg.graphql.model.OrganizationGQO;
+import io.github.conanchen.personorg.graphql.model.PersonGQO;
 import io.github.conanchen.zommon.graphql.model.NodeGQO;
 
-public class ChildcareGQO implements LocalBusinessGQO, NodeGQO{
+import java.util.Collection;
+
+public class ChildcareGQO implements OrganizationGQO, LocalBusinessGQO, NodeGQO{
 
     private String openingHours;
     private String paymentAccepted;
     private String priceRange;
+    private Collection<OrganizationGQO> parentOrganization;
+    private Collection<OrganizationGQO> subOrganizations;
+    private Collection<OrganizationGQO> departments;
+    private Collection<PersonGQO> employees;
+    private Collection<PersonGQO> followees;
+    private Collection<ActionGQO> potentialActions;
+    private Collection<MembershipGQO> members;
+    private MembershipConnectionGQO membersPage;
     @javax.validation.constraints.NotNull
     private String id;
 
     public ChildcareGQO() {
     }
 
-    public ChildcareGQO(String openingHours, String paymentAccepted, String priceRange, String id) {
+    public ChildcareGQO(String openingHours, String paymentAccepted, String priceRange, Collection<OrganizationGQO> parentOrganization, Collection<OrganizationGQO> subOrganizations, Collection<OrganizationGQO> departments, Collection<PersonGQO> employees, Collection<PersonGQO> followees, Collection<ActionGQO> potentialActions, Collection<MembershipGQO> members, MembershipConnectionGQO membersPage, String id) {
         this.openingHours = openingHours;
         this.paymentAccepted = paymentAccepted;
         this.priceRange = priceRange;
+        this.parentOrganization = parentOrganization;
+        this.subOrganizations = subOrganizations;
+        this.departments = departments;
+        this.employees = employees;
+        this.followees = followees;
+        this.potentialActions = potentialActions;
+        this.members = members;
+        this.membersPage = membersPage;
         this.id = id;
     }
 
@@ -41,6 +64,62 @@ public class ChildcareGQO implements LocalBusinessGQO, NodeGQO{
         this.priceRange = priceRange;
     }
 
+    public Collection<OrganizationGQO> getParentOrganization() {
+        return parentOrganization;
+    }
+    public void setParentOrganization(Collection<OrganizationGQO> parentOrganization) {
+        this.parentOrganization = parentOrganization;
+    }
+
+    public Collection<OrganizationGQO> getSubOrganizations() {
+        return subOrganizations;
+    }
+    public void setSubOrganizations(Collection<OrganizationGQO> subOrganizations) {
+        this.subOrganizations = subOrganizations;
+    }
+
+    public Collection<OrganizationGQO> getDepartments() {
+        return departments;
+    }
+    public void setDepartments(Collection<OrganizationGQO> departments) {
+        this.departments = departments;
+    }
+
+    public Collection<PersonGQO> getEmployees() {
+        return employees;
+    }
+    public void setEmployees(Collection<PersonGQO> employees) {
+        this.employees = employees;
+    }
+
+    public Collection<PersonGQO> getFollowees() {
+        return followees;
+    }
+    public void setFollowees(Collection<PersonGQO> followees) {
+        this.followees = followees;
+    }
+
+    public Collection<ActionGQO> getPotentialActions() {
+        return potentialActions;
+    }
+    public void setPotentialActions(Collection<ActionGQO> potentialActions) {
+        this.potentialActions = potentialActions;
+    }
+
+    public Collection<MembershipGQO> getMembers() {
+        return members;
+    }
+    public void setMembers(Collection<MembershipGQO> members) {
+        this.members = members;
+    }
+
+    public MembershipConnectionGQO getMembersPage() {
+        return membersPage;
+    }
+    public void setMembersPage(MembershipConnectionGQO membersPage) {
+        this.membersPage = membersPage;
+    }
+
     public String getId() {
         return id;
     }
@@ -48,4 +127,8 @@ public class ChildcareGQO implements LocalBusinessGQO, NodeGQO{
         this.id = id;
     }
 
+    public MembershipConnectionGQO membersPage(String after, Integer first, Integer before, String last, String personId) throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }
