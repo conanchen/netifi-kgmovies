961aad7cd51f99f9f7c5e9c5508d40296a7b3ff8
陈朵拉 <conan8chan@yahoo.com>
2020-03-02 17:50:29 +0800

    refactor(pagination): pagination and storeconnection comment

---
+++ gapi-gateway/src/main/java/io/github/conanchen/place/graphql/model/StoreConnectionGQO.java
@@ -0,0 +1,52 @@
+package io.github.conanchen.place.graphql.model;
+
+import java.util.*;
+import io.github.conanchen.place.graphql.api.*;
+
+public class StoreConnectionGQO {
+
+    @javax.validation.constraints.NotNull
+    private PageInfoGQO pageInfo;
+    private Collection<StoreEdgeGQO> edges;
+    private Integer totalCount;
+    private Collection<StoreGQO> films;
+
+    public StoreConnectionGQO() {
+    }
+
+    public StoreConnectionGQO(PageInfoGQO pageInfo, Collection<StoreEdgeGQO> edges, Integer totalCount, Collection<StoreGQO> films) {
+        this.pageInfo = pageInfo;
+        this.edges = edges;
+        this.totalCount = totalCount;
+        this.films = films;
+    }
+
+    public PageInfoGQO getPageInfo() {
+        return pageInfo;
+    }
+    public void setPageInfo(PageInfoGQO pageInfo) {
+        this.pageInfo = pageInfo;
+    }
+
+    public Collection<StoreEdgeGQO> getEdges() {
+        return edges;
+    }
+    public void setEdges(Collection<StoreEdgeGQO> edges) {
+        this.edges = edges;
+    }
+
+    public Integer getTotalCount() {
+        return totalCount;
+    }
+    public void setTotalCount(Integer totalCount) {
+        this.totalCount = totalCount;
+    }
+
+    public Collection<StoreGQO> getFilms() {
+        return films;
+    }
+    public void setFilms(Collection<StoreGQO> films) {
+        this.films = films;
+    }
+
+}
