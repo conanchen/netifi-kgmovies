a963c8b58e7a413c2c5d75847ea65190531bd2ef
陈朵拉 <conan8chan@yahoo.com>
2020-03-29 16:32:40 +0800

    动态字段

--- gapi-gateway/src/main/java/io/github/config/CustomTypeDefinitionFactory.java
+++ gapi-gateway/src/main/java/io/github/config/CustomTypeDefinitionFactory.java
@@ -7,30 +7,48 @@ import graphql.language.*;
 import java.util.List;
 
 class CustomTypeDefinitionFactory implements TypeDefinitionFactory {
-  public List<Definition<?>> create(final List<Definition<?>> existing) {
-    return Lists.newArrayList(
+    public List<Definition<?>> create(final List<Definition<?>> existing) {
+        return Lists.newArrayList(
 
-            ObjectTypeDefinition.newObjectTypeDefinition()
-              .name("MyType")
-              .fieldDefinition(new FieldDefinition("myField", new TypeName("String")))
-              .fieldDefinition(new FieldDefinition("name", new TypeName("String")))
-              .build(),
-            ObjectTypeDefinition.newObjectTypeDefinition()
-              .name("HisType")
-              .fieldDefinition(new FieldDefinition("hisField", new TypeName("String")))
-              .fieldDefinition(new FieldDefinition("name", new TypeName("String")))
-              .build(),
+                ObjectTypeDefinition.newObjectTypeDefinition()
+                        .name("MyType")
+                        .fieldDefinition(new FieldDefinition("myField", new TypeName("String")))
+                        .fieldDefinition(new FieldDefinition("name", new TypeName("String")))
+                        .build(),
 
-            //TODO : dynmically add resolvers?
-            ObjectTypeDefinition.newObjectTypeDefinition()
-              .name("Query")
-              .fieldDefinition(FieldDefinition.newFieldDefinition().name("findMyType")
-                      .type(new TypeName("MyType"))
-                      .inputValueDefinition(InputValueDefinition.newInputValueDefinition().name("id")
-                              .type(new TypeName("String")).build()).build())
-              .build()
+                ObjectTypeDefinition.newObjectTypeDefinition()
+                        .name("HisType")
+                        .fieldDefinition(new FieldDefinition("hisField", new TypeName("String")))
+                        .fieldDefinition(FieldDefinition.newFieldDefinition()
+                                .name("name")
+                                .type(new TypeName("String"))
+                                .description(new Description("【名称】该项目名称", null, false))
+                                .build())
+                        .build(),
 
-    );
+                //TODO : dynmically add resolvers?
+                ObjectTypeDefinition.newObjectTypeDefinition()
+                        .name("Query")
+                        .fieldDefinition(FieldDefinition.newFieldDefinition().name("findMyType")
+                                .type(new TypeName("MyType"))
+                                .inputValueDefinition(InputValueDefinition.newInputValueDefinition().name("id")
+                                        .type(new TypeName("String")).build()).build())
+                        .fieldDefinition(FieldDefinition.newFieldDefinition().name("searchHisType")
+                                .type(new ListType(new TypeName("HisType")))
+                                .inputValueDefinitions(Lists.newArrayList(
+                                        InputValueDefinition.newInputValueDefinition()
+                                                .name("id")
+                                                .type(new TypeName("String"))
+                                                .build(),
+                                        InputValueDefinition.newInputValueDefinition()
+                                                .name("page")
+                                                .type(new TypeName("Int"))
+                                                .build()))
+                                .build())
 
-  }
+                        .build()
+
+        );
+
+    }
 }
--- gapi-gateway/src/main/java/io/github/config/MyTypeQueriesResolver.java
+++ gapi-gateway/src/main/java/io/github/config/MyTypeQueriesResolver.java
@@ -1,19 +1,45 @@
 package io.github.config;
 
+import com.google.common.collect.Lists;
 import graphql.kickstart.tools.GraphQLQueryResolver;
 import graphql.schema.DataFetchingEnvironment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
 
 @Slf4j
 @Service
 public class MyTypeQueriesResolver implements GraphQLQueryResolver {
 
     public MyType findMyType(String id, DataFetchingEnvironment env) throws Exception {
-        MyType myType =  new MyType();
-        myType.put("myField","my field value");
-        myType.put("name","my name value");
+        MyType myType = new MyType();
+        myType.put("myField", "my field value");
+        myType.put("name", "my name value");
         return myType;
 
+    }
+
+    public Collection<HashMap> searchHisType(String id, Integer page, DataFetchingEnvironment env) throws Exception {
+        return Lists.newArrayList(
+                new HashMap() {{
+                    put("hisField", "his field value");
+                    put("name", "his name value");
+                }},
+                new HashMap() {{
+                    put("hisField", "his field value1");
+                    put("name", "his name value1");
+                }},
+                new HashMap() {{
+                    put("hisField", "his field value2");
+                    put("name", "his name value3");
+                }},
+                new HashMap() {{
+                    put("hisField", "his field value4");
+                    put("name", "his name value4");
+                }}
+        );
     }
 }
